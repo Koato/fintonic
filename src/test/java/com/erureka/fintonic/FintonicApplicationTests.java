a0265bebd71b72b9d85fb395e68fc87f83e30c4d
Andy Gomez <andy.gomez@atmira.com>
2024-06-17 06:52:41 -0500

    docs(init): iniciar proyecto

---
+++ src/test/java/com/erureka/fintonic/FintonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erureka.fintonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
